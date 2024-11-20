fed7e9f5724a74aca945712e2f8c815e60375527
aruk04 <141922774+aruk04@users.noreply.github.com>
2024-11-20 22:26:13 +0530

    Update and rename update.py to update_business.py

--- update_business.py
+++ update_business.py
@@ -2,10 +2,10 @@ import pandas as pd #type: ignore
 import streamlit as st #type: ignore
 from database import view_all_data, view_only_business_names, get_business, edit_business_data
 
-def update():
+def update(c, mydb):
     # Fetch all data from the Business table
     result = view_all_data()
-    df = pd.DataFrame(result, columns=['Business ID', 'Business Name', 'Legal Name', 'Founder Name', 'Business Type', 'Official Email', 'Phone Number'])
+    df = pd.DataFrame(result, columns=['Business ID', 'Business Name', 'Last Name', 'First Name', 'Business Type', 'Official Email', 'Phone Number'])
     
     with st.expander("Current Businesses"):
         st.dataframe(df)
@@ -18,8 +18,8 @@ def update():
     if selected_result:
         business_id = selected_result[0][0]
         business_name = selected_result[0][1]
-        legal_name = selected_result[0][2]
-        founder_name = selected_result[0][3]
+        last_name = selected_result[0][2]
+        first_name = selected_result[0][3]
         business_type = selected_result[0][4]
         official_email = selected_result[0][5]
         phone_number = selected_result[0][6]
@@ -29,8 +29,8 @@ def update():
         with col1:
             new_business_id = st.text_input("Business ID:", business_id)
             new_business_name = st.text_input("Business Name:", business_name)
-            new_legal_name = st.text_input("Legal Name:", legal_name)
-            new_founder_name = st.text_input("Founder Name:", founder_name)
+            new_last_name = st.text_input("Last Name:", last_name)
+            new_first_name = st.text_input("First Name:", first_name)
         with col2:
             new_business_type = st.selectbox("Business Type:", ["Fashion", "Travel", "Sports", "Food"], index=["Fashion", "Travel", "Sports", "Food"].index(business_type))
             new_official_email = st.text_input("Official Email:", official_email)
@@ -38,11 +38,11 @@ def update():
         
         # Button to update business details
         if st.button("Update Business"):
-            edit_business_data(new_business_id, new_business_name, new_legal_name, new_founder_name, new_business_type, new_official_email, new_phone_number, business_id, business_name, legal_name, founder_name, business_type, official_email, phone_number)
+            edit_business_data(new_business_id, new_business_name,  new_last_name, new_first_name, new_business_type, new_official_email, new_phone_number, business_id, business_name, last_name, first_name, business_type, official_email, phone_number)
             st.success(f"Successfully updated: {business_name} to {new_business_name}")
 
     # Display updated data
     result2 = view_all_data()
-    df2 = pd.DataFrame(result2, columns=['Business ID', 'Business Name', 'Legal Name', 'Founder Name', 'Business Type', 'Official Email', 'Phone Number'])
+    df2 = pd.DataFrame(result2, columns=['Business ID', 'Business Name','Last Name', 'First Name', 'Business Type', 'Official Email', 'Phone Number'])
     with st.expander("Updated Data"):
         st.dataframe(df2)
