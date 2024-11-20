bb8caee4208efbed060b9f3c02bdf12103ca70d2
aruk04 <141922774+aruk04@users.noreply.github.com>
2024-11-20 22:23:27 +0530

    Update create.py

--- create.py
+++ create.py
@@ -6,14 +6,15 @@ def create():
     with col1:
         b_id = st.text_input("Business ID:")
         b_name = st.text_input("Business Name:")
-        l_name = st.text_input("Legal Name:")
-        f_name = st.text_input("Founder Name:")
+        f_name = st.text_input("First Name:")
+        l_name = st.text_input("Last Name:")
     with col2:
-        b_type = st.selectbox("Business Type", ["Fashion", "Travel", "Sports", "Food"])  # Adjust as needed
+        b_type = st.selectbox("Business Type", ["Fashion", "Travel", "Sports", "Food"])  
         oo_mail = st.text_input("Official E-mail:")
         phone = st.text_input("Phone Number:")
 
     if st.button("Add Business"):
         # Call the add_data function with the inputs for the Business table
-        add_data(b_id, b_name, l_name, f_name, b_type, oo_mail, phone)
+        add_data(b_id, b_name,l_name, f_name, b_type, oo_mail, phone)
         st.success(f"Successfully added Business: {b_name}")
+
